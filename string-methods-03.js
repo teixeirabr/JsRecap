1b9b5808e858ebe235a420a3eef648ec2c9377b5
andre <andrelaprov@gmail.com>
2021-10-04 17:55:03 -0400

    strings

---
+++ string-methods-03.js
@@ -0,0 +1,14 @@
+/*
+
+Write a function called "getLengthOfThreeWords".
+
+Given 3 words, "getLengthOfThreeWords" returns the sum of their lengths.
+
+var output = getLengthOfThreeWords('some', 'other', 'words');
+console.log(output); // --> 14
+
+*/
+
+getLengthOfThreeWords = (word1, word2, word3) => {
+  return word1.length + word2.length + word3.length;
+};
