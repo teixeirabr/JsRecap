ad2ab298ad45d52f36e5d8be0bc52b93c43d0671
andre <andrelaprov@gmail.com>
2021-10-04 00:04:45 -0400

    array-methods-02

--- array-methods-02.js
+++ array-methods-02.js
@@ -19,4 +19,22 @@ addToFront = (arr, element) => {
   return arr;
 };
 
-console.log("hi", addToFront([1, 2], 3)
+/*
+
+Write a function called "addToBack".
+
+Given an array and an element, "addToBack" returns the given array with the given element added to the end.
+
+Notes:
+
+It should be the SAME array, not a new array.
+In order to do this you should be familiar with the 'push' method.
+var output = addToBack([1, 2], 3);
+console.log(output); // -> [1, 2, 3]
+
+*/
+
+addToBack = (arr, element) => {
+  arr.push(element);
+  return arr;
+};
