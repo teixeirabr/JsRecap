b84cb84ad4e88ea41b4aa19c100a064aa073f57d
andre <andrelaprov@gmail.com>
2021-10-08 23:27:37 -0400

    countNumberOfKeys

--- objects-05.js
+++ objects-05.js
@@ -32,4 +32,37 @@ removeEvenValues = (obj) => {
   }
   return obj;
 };
-console.log(removeEvenValues(obj));
+// console.log(removeEvenValues(obj));
+
+/*
+
+countNumberOfKeys
+Write a function called "countNumberOfKeys".
+
+Given an object, "countNumberOfKeys" returns how many properties the given object has.
+
+var obj = {
+  a: 1,
+  b: 2,
+  c: 3
+};
+var output = countNumberOfKeys(obj);
+console.log(output); // --> 3
+
+*/
+
+var obj = {
+  a: 1,
+  b: 2,
+  c: 3,
+};
+
+countNumberOfKeys = (obj) => {
+  let count = 0;
+  for (const key in obj) {
+    count = obj[key]++;
+  }
+  return count;
+};
+
+console.log(countNumberOfKeys(obj));
