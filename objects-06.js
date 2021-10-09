43c9457b90eed75cc8f829f163db4b538ab8f46e
andre <andrelaprov@gmail.com>
2021-10-09 00:26:14 -0400

    removeNumberValues

---
+++ objects-06.js
@@ -0,0 +1,66 @@
+/*
+
+removeArrayValues
+Write a function called "removeArrayValues".
+
+Given an object, "removeArrayValues" removes any properties whose values are arrays.
+
+var obj = {
+  a: [1, 3, 4],
+  b: 2,
+  c: ['hi', 'there']
+}
+removeArrayValues(obj);
+console.log(obj); // --> { b: 2 } 
+
+*/
+
+// var obj = {
+//   a: [1, 3, 4],
+//   b: 2,
+//   c: ["hi", "there"],
+// };
+
+removeArrayValues = (obj) => {
+  for (let key in obj) {
+    if (typeof obj[key] === "object") {
+      delete obj[key];
+    }
+  }
+  return obj;
+};
+
+// console.log(removeArrayValues(obj));
+
+/*
+removeNumberValues
+Write a function called "removeNumberValues".
+
+Given an object, "removeNumberValues" removes any properties whose values are numbers.
+
+var obj = {
+  a: 2,
+  b: 'remaining',
+  c: 4
+};
+removeNumberValues(obj);
+console.log(obj); // --> { b: 'remaining' }
+
+*/
+
+var obj = {
+  a: 2,
+  b: "remaining",
+  c: 4,
+};
+
+removeNumberValues = (obj) => {
+  for (let key in obj) {
+    if (typeof obj[key] === "number") {
+      delete obj[key];
+    }
+  }
+  return obj;
+};
+
+console.log(removeNumberValues(obj));
