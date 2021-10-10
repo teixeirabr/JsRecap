b8266d94698ca2d1d128e7dfb9163ea5a1ce69c2
andre <andrelaprov@gmail.com>
2021-10-09 22:34:45 -0400

    remove double spaces with split join

--- array-string-methods-02.js
+++ array-string-methods-02.js
@@ -15,15 +15,18 @@ In order to do this problem, you should be familiar with "String.split", and "Ar
 
 let doubleSpaces = "string  with  double  spaces";
 
+// convertDoubleSpaceToSingle = (str) => {
+//   let arrayString = str.split("");
+//   for (let i = 0; i < arrayString.length; i++) {
+//     if (arrayString[i] === " " && arrayString[i + 1] === " ") {
+//       delete arrayString[i];
+//     }
+//   }
+//   return arrayString.join("");
+// };
+
 convertDoubleSpaceToSingle = (str) => {
-  let arrayString = str.split("");
-  console.log("first console", arrayString);
-  for (let i = 0; i < arrayString.length; i++) {
-    if (arrayString[i] === " " && arrayString[i + 1] === " ") {
-      delete arrayString[i];
-    }
-  }
-  return arrayString.join("");
+  return str.split("  ").join(" ");
 };
 
 console.log("hi", convertDoubleSpaceToSingle(doubleSpaces));
