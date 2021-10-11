225d1b0c281a480602ed83ccfe9943314b15cd43
andre <andrelaprov@gmail.com>
2021-10-11 18:15:01 -0400

    cond 09 finished

--- conditionals-09.js
+++ conditionals-09.js
@@ -25,3 +25,30 @@ getLongestOfThreeWords = (word1, word2, word3) => {
 };
 
 console.log(getLongestOfThreeWords("these", "three", "words"));
+
+/*
+findShortestOfThreeWords
+Submitted on Today at 6:04 PM
+Write a function called "findShortestOfThreeWords".
+
+Given 3 strings, "findShortestOfThreeWords" returns the shortest of the given strings.
+
+Notes:
+
+If there are ties, it should return the first word in the parameters list.
+var output = findShortestOfThreeWords('a', 'two', 'three');
+console.log(output); // --> 'a'
+
+*/
+
+function findShortestOfThreeWords(word1, word2, word3) {
+  // your code here
+  let words = [word1, word2, word3];
+  let shortestWord = words[0];
+  for (i = 0; i < words.length; i++) {
+    if (words[i].length < shortestWord.length) {
+      shortestWord = words[i];
+    }
+  }
+  return shortestWord;
+}
