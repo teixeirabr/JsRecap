09047c40a73ab5376fb483f3442009a34caea72d
andre <andrelaprov@gmail.com>
2021-10-10 23:14:39 -0400

    conditionals 7

--- conditionals-07.js
+++ conditionals-07.js
@@ -15,4 +15,47 @@ areValidCredentials = (name, password) => {
   } else return false;
 };
 
-console.log(areValidCredentials("andre", 12345678));
+// console.log(areValidCredentials("andre", 12345678));
+
+/*
+
+findMinLengthOfThreeWords
+Write a function called "findMinLengthOfThreeWords".
+
+Given 3 words, "findMinLengthOfThreeWords" returns the length of the shortest word.
+
+var output = findMinLengthOfThreeWords('a', 'be', 'see');
+console.log(output); // --> 1
+
+*/
+
+findMinLengthOfThreeWords = (word1, word2, word3) => {
+  let check = [word1.length, word2.lenth, word3.length];
+  check.sort();
+  const min = check[0];
+
+  return min;
+};
+
+// console.log(findMinLengthOfThreeWords("a", "be", "see"));
+
+/*
+
+indMaxLengthOfThreeWords
+Write a function called "findMaxLengthOfThreeWords".
+
+Given 3 words, "findMaxLengthOfThreeWords" returns the length of the longest word.
+
+var output = findMaxLengthOfThreeWords('a', 'be', 'see');
+console.log(output); // --> 3
+
+*/
+
+findMaxLengthOfThreeWords = (word1, word2, word3) => {
+  let check = [word1.length, word2.length, word3.length];
+  console.log("array length", check.length);
+  check.sort();
+  return check[check.length - 1];
+};
+
+console.log(findMaxLengthOfThreeWords("a", "be", "see"));
