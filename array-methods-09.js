2010f3117521e5c30b5ce789fd5cfd8fc3f2f262
andre <andrelaprov@gmail.com>
2021-10-13 00:36:30 -0400

    to be finished

--- array-methods-09.js
+++ array-methods-09.js
@@ -17,4 +17,13 @@ filterOddLengthWords = (words) => {
   return array;
 };
 
-console.log(filterOddLengthWords(["there", "it", "is", "now"]));
+/*
+Write a function called "filterEvenLengthWords".
+
+Given an array of strings, "filterEvenLengthWords" returns an array containing only the elements of the given array whose length is an even number.
+
+var output = filterEvenLengthWords(['word', 'words', 'word', 'words']);
+console.log(output); // --> ['word', 'word']
+*/
+
+filterEvenLengthWords = (words) => {};
