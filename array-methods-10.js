682e5a312f2fad20f2987a4d3fbd6c5317e3ab2a
andre <andrelaprov@gmail.com>
2021-10-14 15:53:18 -0400

    method 10

--- array-methods-10.js
+++ array-methods-10.js
@@ -15,4 +15,52 @@ squareElements = (array) => {
   return square;
 };
 
-console.log(squareElements([1, 2, 3]));
+// console.log(squareElements([1, 2, 3]));
+
+/*
+
+Write a function called "filterOddElements".
+
+Given an array of numbers, "filterOddElements" returns an array containing only the odd numbers of the given array.
+
+var output = filterOddElements([1, 2, 3, 4, 5]);
+console.log(output); // --> [1, 3, 5]
+
+/*/
+
+filterOddElements = (arr) => {
+  let filtered = arr.filter((e) => e % 2 !== 0);
+  return filtered;
+};
+
+// console.log(filterOddElements([1, 2, 3, 4, 5]));
+
+/*
+
+computeProductOfAllElements
+Write a function called "computeProductOfAllElements".
+
+Given an array of numbers, "computeProductOfAllElements" returns the products of all the elements in the given array.
+
+Notes:
+
+If given array is empty, it should return 0.
+var output = computeProductOfAllElements([2, 5, 6]);
+console.log(output); // --> 60
+
+*/
+
+computeProductOfAllElements = (arr) => {
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  let p = 1;
+
+  for (let i = 0; i < arr.length; i++) {
+    p *= arr[i];
+  }
+  return p;
+};
+
+console.log(computeProductOfAllElements([2, 5, 6]));
