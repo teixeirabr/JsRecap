aeff6c69e8ea56b2213f5af9cd78b4f5e39d74c1
andre <andrelaprov@gmail.com>
2021-10-17 00:44:20 -0400

    obj 07

---
+++ objects-07.js
@@ -0,0 +1,40 @@
+/*
+
+getElementsThatEqual10AtProperty
+Write a function called "getElementsThatEqual10AtProperty".
+
+Given an object and a key, "getElementsThatEqual10AtProperty" returns an array containing all the elements of the array located at the given key that are equal to ten.
+
+Notes:
+
+If the array is empty, it should return an empty array.
+If the array contains no elements that are equal to 10, it should return an empty array.
+If the property at the given key is not an array, it should return an empty array.
+If there is no property at the key, it should return an empty array.
+var obj = {
+  key: [1000, 10, 50, 10]
+};
+var output = getElementsThatEqual10AtProperty(obj, 'key');
+console.log(output); // --> [10, 10]
+
+*/
+
+function getElementsThatEqual10AtProperty(obj, key) {
+  if (obj[key] === undefined) {
+    return [];
+  }
+
+  let result = [];
+
+  for (let i = 0; i < obj[key].length; i++) {
+    if (obj[key][i] === 10) {
+      result.push(obj[key][i]);
+    }
+  }
+  return result;
+}
+
+var obj = {
+  key: [1000, 10, 50, 10],
+};
+console.log(getElementsThatEqual10AtProperty(obj, "key"));
