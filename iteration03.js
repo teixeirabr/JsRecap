d00149726a3eb1b2504f03fa407fbd83b96818ce
andre <andrelaprov@gmail.com>
2021-10-21 00:11:22 -0400

    iteration 03

---
+++ iteration03.js
@@ -0,0 +1,24 @@
+/*
+computeSummationToN
+Write a function called "computeSummationToN".
+
+Given a number, "computeSummationToN" returns the sum of sequential numbers leading up to the given number, beginning at 0.
+
+Notes:
+
+If n = 4, it should calculate the sum of 1 + 2 + 3 + 4, and return 10.
+var output = computeSummationToN(6);
+console.log(output); // --> 21
+
+*/
+function computeSummationToN(n) {
+  let count = 0;
+
+  for (let i = 0; i <= n; i++) {
+    count += i;
+  }
+
+  return count;
+}
+
+console.log(computeSummationToN(6));
