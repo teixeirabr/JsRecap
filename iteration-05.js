bd01dc66ab45438d08822c7707615abe5ed60e94
andre <andrelaprov@gmail.com>
2021-10-21 23:57:36 -0400

    it 05

--- iteration-05.js
+++ iteration-05.js
@@ -14,11 +14,24 @@ console.log(output); // --> 28
 */
 
 multiply = (num1, num2) => {
+  let resultIsPositive = true;
+  if ((num1 > 0 && num2 < 0) || (num2 > 0 && num1 < 0)) {
+    resultIsPositive = false;
+  }
+
+  num1 = Math.abs(num1);
+  num2 = Math.abs(num2);
+
   let result = 0;
   for (let i = 0; i < num2; i++) {
     result += num1;
   }
+
+  if (resultIsPositive) {
+    return result;
+  } else {
+    return -result;
+  }
 };
 
-var output = multiply(4, 7);
-console.log(output); // --> 28
+console.log(multiply(4, 7));
