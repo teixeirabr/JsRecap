119ed5d96e7b0e4842e250c3fb1f2359d1aefd62
Andre <andre@A-MacBook-Air.local>
2022-05-09 22:47:19 -0400

    array methods 15

---
+++ array-methods-15.js
@@ -0,0 +1,12 @@
+/*/
+Write a function called "findShortestWordAmongMixedElements".
+
+Given an array, "findShortestWordAmongMixedElements" returns the shortest string within the given array.
+
+Notes:
+
+If there are ties, it should return the first element to appear in the given array.
+Expect the given array to have values other than strings.
+If the given array is empty, it should return an empty string.
+If the given array contains no strings, it should return an empty string.
+/*/
